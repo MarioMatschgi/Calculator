1bb572a9de6d9a86d70cc8f5c413ed0d7c8bcced
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2021-11-13 22:07:51 +0100

    Setup plain project

---
+++ Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by Mario Elsnig on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
